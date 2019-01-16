6195e577b36a1fc2ac40eed1ae9aba0e6cdb1e05
Minsoo Kwak <marcosdeseul@gmail.com>
2019-01-17 02:58:37 +0800

    add for-loop

---
+++ for-loop.py
@@ -0,0 +1,20 @@
+# for-loop with list
+names = ["marco", "irene", "scott", "joe"]
+for n in names:
+    # print("Hi, " + n)
+    pass
+
+# for-loop with range
+for index in range(10):
+    # print(index)
+    pass
+
+for index in range(2, 10):
+    # print(index)
+    pass
+
+# for-loop with index
+names = ["marco", "irene", "scott", "joe"]
+for index, name in enumerate(names):
+    print(index, name)
+    # pass
