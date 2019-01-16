55f1ce32b6f919e2255a6995f4f6998d136be436
Minsoo Kwak <marcosdeseul@gmail.com>
2019-01-17 03:40:59 +0800

    add function

---
+++ function.py
@@ -0,0 +1,40 @@
+## types of function
+# parameter O, return value O
+def sum(a, b):
+    return a + b
+result_sum = sum(1, 2)
+# print(result_sum)
+
+# parameter O, return value X
+def hi(name):
+    print("Hello, " + name)
+# hi("marco")
+
+# parameter X, return value O
+def three():
+    return 3
+result_three = three()
+# print(result_three)
+
+# parameter X, return value X
+def hello():
+    print("Hello!")
+# hello()
+
+
+## why to use function?
+a = "marco"
+# print("Hi, " + a)
+hi(a)
+
+b = "irene"
+# print("Hi, " + b)
+hi(b)
+
+c = "scott"
+# print("Hi, " + c)
+hi(c)
+
+d = "joe"
+# print("Hi, " + d)
+hi(d)
