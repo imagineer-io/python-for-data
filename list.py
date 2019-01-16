e0af71453c841d89c83184fbb6b7135f2bef00dd
Minsoo Kwak <marcosdeseul@gmail.com>
2019-01-17 01:44:04 +0800

    add list and tuple

---
+++ list.py
@@ -0,0 +1,15 @@
+# types of list
+numbers = [1, 2, 3, 4, 5]
+countries = ["Korea", "Japan", "China"]
+
+# append
+countries.append("Singapore")
+print(countries)
+
+# remove
+countries.remove("Japan")
+print(countries)
+
+# access
+print(countries[0])
+print(countries[1])
---
+++ tuple.py
@@ -0,0 +1,7 @@
+# types of tuples
+numbers_list = [1, 2, 3, 4] # list
+numbers = (1, 2, 3, 4)
+countries = ("Korea", "Japan", "China")
+
+# access
+print(numbers[0])
